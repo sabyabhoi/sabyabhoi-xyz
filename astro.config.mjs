f68e1c699ae159c2ae47b9add772f4f2a91b7681
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2023-01-05 00:24:17 +0530

    Add toggle theme functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()]
